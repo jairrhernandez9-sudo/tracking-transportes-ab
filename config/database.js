bc844a7810d3b323502321c1a6e951df68b668fb
Tu Nombrejairrhernandez9-sudo <jairrhernandez9@gmail.com>
2026-03-25 02:11:27 -0600

    modified:   config/database.js

--- config/database.js
+++ config/database.js
@@ -9,7 +9,8 @@ const pool = mysql.createPool({
   port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  timezone: '+00:00'
 });
 
 const promisePool = pool.promise();
